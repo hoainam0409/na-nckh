b1f5a694775be440a392dcde821d7611f82fd94a
hoainam0409 <62093966+hoainam0409@users.noreply.github.com>
2022-06-22 17:31:34 +0700

    22/6 code danh mục

--- client/src/contexts/constant.js
+++ client/src/contexts/constant.js
@@ -2,9 +2,26 @@ export const apiUrl = 'http://localhost:5000'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'quanly-nckh'
 
+//Thông báo chung
 export const TB_LOADED_SUCCESS = 'TB_LOADED_SUCCESS'
 export const TB_LOADED_FAIL = 'TB_LOADED_FAIL'
 export const TB_ADD = 'TB_ADD'
 export const DELETE_TB = 'DELETE_TB'
 export const UPDATE_TB = 'UPDATE_TB'
 export const FIND_TB = 'FIND_TB'
+
+//Cấp đề tài
+export const CAPDETAI_LOADED_SUCCESS = 'CAPDETAI_LOADED_SUCCESS'
+export const CAPDETAI_LOADED_FAIL = 'CAPDETAI_LOADED_FAIL'
+export const CAPDETAI_ADD = 'CAPDETAI_ADD'
+export const DELETE_CAPDETAI = 'DELETE_CAPDETAI'
+export const UPDATE_CAPDETAI = 'UPDATE_CAPDETAI'
+export const FIND_CAPDETAI = 'FIND_CAPDETAI'
+
+//Đợt đăng ký đề tài
+export const DOTDANGKY_LOADED_SUCCESS = 'DOTDANGKY_LOADED_SUCCESS'
+export const DOTDANGKY_LOADED_FAIL = 'DOTDANGKY_LOADED_FAIL'
+export const DOTDANGKY_ADD = 'DOTDANGKY_ADD'
+export const DELETE_DOTDANGKY = 'DELETE_DOTDANGKY'
+export const UPDATE_DOTDANGKY = 'UPDATE_DOTDANGKY'
+export const FIND_DOTDANGKY = 'FIND_DOTDANGKY'
