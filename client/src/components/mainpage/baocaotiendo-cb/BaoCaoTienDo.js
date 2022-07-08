42ac27d395aa2a17f34680e4a3b97a8a3b122b9f
hoainam0409 <62093966+hoainam0409@users.noreply.github.com>
2022-07-08 17:12:34 +0700

    1

--- client/src/components/mainpage/baocaotiendo-cb/BaoCaoTienDo.js
+++ client/src/components/mainpage/baocaotiendo-cb/BaoCaoTienDo.js
@@ -202,7 +202,202 @@ const BaoCaoTienDo = () => {
 
             </div>
           </div>
-          <div></div>
+          <div>
+            <h1>SẢN PHẨM VÀ ỨNG DỤNG</h1>
+            <div className="loaisanpham">
+              <h2>Sản phẩm khoa học </h2>
+              <div className="sanpham">
+                <div><h3>Bài báo, báo cáo</h3>
+                  <Button>Thêm mới</Button>
+                  <Table borderless bordered hover style={{ cursor: "pointer" }}>
+                    <thead>
+                      <tr>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          STT
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Loại bài báo, báo cáo
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Số lượng
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Mô tả
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Chức năng
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                  </Table>
+
+                </div>
+                <div> <h3>Sách, giáo trình khoa học</h3>
+                  <Button>Thêm mới</Button>
+                  <Table borderless bordered hover style={{ cursor: "pointer" }}>
+                    <thead>
+                      <tr>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          STT
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Loại sách, giáo trình khoa học
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Số lượng
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Mô tả
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Chức năng
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                  </Table>
+                </div>
+                <div><h3>Sản phẩm khoa học khác</h3>
+                  <Button>Thêm mới</Button>
+                  <Table borderless bordered hover style={{ cursor: "pointer" }}>
+                    <thead>
+                      <tr>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          STT
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Loại sản phẩm khoa học khác
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Số lượng
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Mô tả
+                        </th>
+                        <th style={{ textAlign: "center", color: "#495057" }}>
+                          Chức năng
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                  </Table>
+                </div>
+              </div>
+            </div>
+            <div className="loaisanpham">
+              <h2>Sản phẩm đào tạo </h2>
+              <Button>Thêm mới</Button>
+              <Table borderless bordered hover style={{ cursor: "pointer" }}>
+                <thead>
+                  <tr>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      STT
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Loại sản phẩm
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Số lượng đăng ký
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Đã thực hiện
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Chức năng
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </Table>
+
+            </div>
+            <div className="loaisanpham">
+              <h2>Sản phẩm ứng dụng </h2>
+              <Button>Thêm mới</Button>
+              <Table borderless bordered hover style={{ cursor: "pointer" }}>
+                <thead>
+                  <tr>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      STT
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Loại sản phẩm
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Số lượng đăng ký
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Đã thực hiện
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Chức năng
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </Table>
+            </div>
+            <div className="loaisanpham">
+              <h2>Sản phẩm khác</h2>
+              <Button>Thêm mới</Button>
+              <Table borderless bordered hover style={{ cursor: "pointer" }}>
+                <thead>
+                  <tr>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      STT
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Loại sản phẩm
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Số lượng đăng ký
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Đã thực hiện
+                    </th>
+                    <th style={{ textAlign: "center", color: "#495057" }}>
+                      Chức năng
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                </tbody>
+              </Table>
+            </div>
+          </div>
+          <div>
+            <h1>KẾ HOẠCH THỰC HIỆN NGHIÊN CỨU</h1>
+            <Button>Thêm mới</Button>
+            <Table borderless bordered hover style={{ cursor: "pointer" }}>
+              <thead>
+                <tr>
+                  <th style={{ textAlign: "center", color: "#495057" }}>
+                    STT
+                  </th>
+                  <th style={{ textAlign: "center", color: "#495057" }}>
+                    Nội dung theo thuyết minh
+                  </th>
+                  <th style={{ textAlign: "center", color: "#495057" }}>
+                    Ngày bắt đầu
+                  </th>
+                  <th style={{ textAlign: "center", color: "#495057" }}>
+                    Ngày kết thúc
+                  </th>
+                  <th style={{ textAlign: "center", color: "#495057" }}>
+                    Chức năng
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </Table>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="primary" type="submit">
